4f3285bc9bf55ffadc0ab9e85e4d7bd9f51972ae
Jonathan Bouzekri <jonathan.bouzekri@gmail.com>
2021-06-28 17:09:25 +0200

    remove useless code

--- 04_vdm_async/src/Fanout/Message/FanoutActionMessage.php
+++ 04_vdm_async/src/Fanout/Message/FanoutActionMessage.php
@@ -6,8 +6,4 @@ use Vdm\Bundle\LibraryBundle\Model\Message;
 
 class FanoutActionMessage extends Message
 {
-    public function __construct($payload = null, array $metadatas = [], array $traces = [])
-    {
-        parent::__construct($payload, $metadatas, $traces);
-    }
 }
